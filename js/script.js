a5c389b0c1eca826269b2d89554f67b8396adc1f
Melina <123626057+Mel1n4@users.noreply.github.com>
2023-09-30 19:26:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ const textLoad = () => {
 }
 textLoad();
 setInterval(textLoad, 6000);
+
+let scrollToTop = document.getElementById("up");
+
+window.onscroll = function () {
+  scroll();
+};
+
+function scroll() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTop.style.display = "flex";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
